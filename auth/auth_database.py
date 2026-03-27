1b75789143905b75a56cfb587cd32296ec4df1b3
Cmoosa <mchavalala55@gmail.com>
2026-03-27 13:35:38 +0200

    Docker container

--- auth/auth_database.py
+++ auth/auth_database.py
@@ -3,18 +3,22 @@ from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
 
-MYSQL_USER = "root"
-MYSQL_PASSWORD = quote_plus("Cmoosa@081")
-MYSQL_HOST = 'localhost'
-MYSQL_PORT = '3306'
-MYSQL_DATABASE = 'fastapi_database'
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = quote_plus(os.getenv("MYSQL_PASSWORD", "rootpassword"))
+MYSQL_HOST = os.getenv("MYSQL_HOST", "db")
+MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "fastapi_database")
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
 ## Connection
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+                          echo=True,
+                          pool_pre_ping=True
+                       )
 
 ## Session
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
